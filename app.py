ce71ced76f9c731172f7624ce41b09cccab3f924
santhi prasanna veeramallu <113976453+santhi77@users.noreply.github.com>
2024-11-15 18:27:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Load the saved model
-model = pickle.load(open(r'linear_regression_model.pkl', 'rb'))
+model = pickle.load(open('linear_regression_model.pkl', 'rb'))
 
 # Set the title of the Streamlit app
 st.title("Salary Prediction App")
